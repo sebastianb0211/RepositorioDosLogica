ad728923863bba3e3a03af6885effdfeba4d818c
507 <507@academicos.cesde.edu.co>
2023-07-12 15:10:57 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entradaTeclado=new Scanner(System.in);
+
+
+
+
+
+
+    }
+}
